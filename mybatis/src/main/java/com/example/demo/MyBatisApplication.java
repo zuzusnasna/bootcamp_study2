5c66129ff10d07312f897e776af20fcd04106301
jusanghyeok <jsmh991102@gmail.com>
2026-09-04 12:43:44 +0900

    오류수정중

--- mybatis/src/main/java/com/example/demo/MyBatisApplication.java
+++ mybatis/src/main/java/com/example/demo/MyBatisApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.mapper.MemberMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class MyBatisApplication implements ApplicationRunner {
 
     @Autowired
@@ -13,6 +17,11 @@ public class MyBatisApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
+        var members = memberMapper.selectAll();
+        log.info("회원 목록 = {}", members);
+        log.info("---------------------------------------------");
+        var member = memberMapper.selectById(3L);
+        log.info("3번 회원 = {}", member);
 
     }
 }
