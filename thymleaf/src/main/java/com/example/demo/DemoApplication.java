bba9e1334ce71e01008a9c11e46a57c2de4d5b13
jusanghyeok <jsmh991102@gmail.com>
2026-09-08 16:08:45 +0900

    Thymeleaf 실습 완료

--- thymleaf/src/main/java/com/example/demo/DemoApplication.java
+++ thymleaf/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * Spring Boot 애플리케이션의 시작점.
+ *
+ * @SpringBootApplication이 포함된 클래스를 실행하면
+ * Spring Boot가 애플리케이션 컨텍스트를 생성하고 내장 서버를 실행한다.
+ */
+@SpringBootApplication // Spring Boot 설정과 컴포넌트 스캔을 활성화한다.
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    /**
+     * 애플리케이션을 실행하는 main 메서드.
+     */
+    public static void main(String[] args) {
+        // Spring Boot 애플리케이션을 시작한다.
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
