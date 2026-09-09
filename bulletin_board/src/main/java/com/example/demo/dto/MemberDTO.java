f4ce16202b419da1b070c34c73759b42398a4438
jusanghyeok <jsmh991102@gmail.com>
2026-09-09 15:54:32 +0900

    Member 객체와 DTO의 차이

--- bulletin_board/src/main/java/com/example/demo/dto/MemberDTO.java
+++ bulletin_board/src/main/java/com/example/demo/dto/MemberDTO.java
@@ -12,3 +12,15 @@ public class MemberDTO {
     private String name;
     private String email;
 }
+
+
+//        Member
+//→ DB와 연결되는 객체(Entity)
+//
+//        MemberDTO
+//→ Controller ↔ Service ↔ View 사이에서 데이터를 전달하는 객체
+
+//-> 화면에 보여줄 내용과 보여주면 안되는 내용 필터링
+//-> 하지만 DB에는 비밀번호를 가지고있어야 하기때문에
+//-> Member 객체에는 비밀번호 포함 (Entity)
+//-> 화면으로 전달될 내용인 MemberDTO에는 비밀번호가 없음
