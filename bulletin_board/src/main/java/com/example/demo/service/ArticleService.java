f4a78b997d03a8550b406eb196b5b88ea53a3f20
jusanghyeok <jsmh991102@gmail.com>
2026-09-09 17:36:56 +0900

    ArticleService 생성 완료

--- bulletin_board/src/main/java/com/example/demo/service/ArticleService.java
+++ bulletin_board/src/main/java/com/example/demo/service/ArticleService.java
@@ -1,9 +1,30 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.ArticleDTO;
+import com.example.demo.model.Article;
+import com.example.demo.model.Member;
+import com.example.demo.repository.ArticleRepository;
+import com.example.demo.repository.AuthorityRepository;
+import com.example.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
+
+    private final MemberRepository memberRepository;
+    private final ArticleRepository articleRepository;
+    private ArticleDTO mapToArticleDTO(Article article){
+        return ArticleDTO.builder()
+                .id(article.getId())
+                .name(article.getMember().getName())
+                .email(article.getMember().getEmail())
+                .memberId(article.getMember().getId())
+                .title(article.getTitle())
+                .description(article.getDescription())
+                .created(article.getCreated())
+                .updated(article.getUpdated())
+                .build();
+    }
 }
