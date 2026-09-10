2e5ff4a3fb34f2857ab7a6a0e62fcbcc23d769b1
jusanghyeok <jsmh991102@gmail.com>
2026-09-10 16:21:06 +0900

    fix: 로그인 사용자 이름 표시 오류 수정

--- bulletin_board/src/main/java/com/example/demo/dto/ArticleForm.java
+++ bulletin_board/src/main/java/com/example/demo/dto/ArticleForm.java
@@ -12,7 +12,7 @@ import lombok.Data;
 // Builder 패턴으로 객체를 생성할 수 있도록 한다.
 @AllArgsConstructor
 // 모든 필드를 매개변수로 받는 생성자를 자동으로 만든다.
-@NotBlank
+
 public class ArticleForm {
 
     // 게시글 수정 시 기존 게시글을 식별하기 위해 사용할 ID
