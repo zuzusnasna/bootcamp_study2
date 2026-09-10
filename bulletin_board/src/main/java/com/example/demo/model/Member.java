f8b964eb86f1119742bc6d094386efa2707f69ca
jusanghyeok <jsmh991102@gmail.com>
2026-09-10 11:50:59 +0900

    bulletin_board: Member 주석 추가

--- bulletin_board/src/main/java/com/example/demo/model/Member.java
+++ bulletin_board/src/main/java/com/example/demo/model/Member.java
@@ -9,19 +9,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//모델 생성
+// 회원 정보를 DB에 저장하기 위한 JPA Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Member {
+
+    // 회원의 기본 키(PK)
     @Id
+    // 회원 객체가 DB에 저장될 때 ID를 자동으로 생성
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 회원 이름
     private String name;
-    private String email;
-    private String password;
 
+    // 로그인에 사용하는 이메일
+    private String email;
+
+    // BCrypt 등으로 암호화해서 저장할 비밀번호
+    private String password;
 }
