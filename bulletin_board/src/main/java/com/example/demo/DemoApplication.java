37f8082b2f350ead865853772813b5ae009e28e0
jusanghyeok <jsmh991102@gmail.com>
2026-09-10 16:43:11 +0900

    docs: 게시판 애플리케이션 실행 코드 주석 보강

--- bulletin_board/src/main/java/com/example/demo/DemoApplication.java
+++ bulletin_board/src/main/java/com/example/demo/DemoApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// Spring Boot 애플리케이션의 시작점이 되는 클래스이다.
 @SpringBootApplication
+// Entity의 생성일과 수정일을 자동으로 기록하는 JPA Auditing 기능을 활성화한다.
 @EnableJpaAuditing
-//@EnableJpaAuditing을 켜야 Article의 @CreatedDate와
-//@LastModifiedDate가 동작합니다.
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    // Java 애플리케이션이 실행될 때 가장 먼저 호출되는 메서드이다.
+    public static void main(String[] args) {
+        // Spring Boot 애플리케이션을 실행하고 Spring 컨테이너를 생성한다.
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
