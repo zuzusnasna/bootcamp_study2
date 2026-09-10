7b2c42ad8f0e765a085f3cdf498b8c1b24cc59fb
jusanghyeok <jsmh991102@gmail.com>
2026-09-10 12:13:49 +0900

    MemberService 에 회원가입 기능추가

--- bulletin_board/src/main/java/com/example/demo/service/MemberService.java
+++ bulletin_board/src/main/java/com/example/demo/service/MemberService.java
@@ -1,13 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.MemberDTO;
+import com.example.demo.dto.MemberForm;
 import com.example.demo.model.Member;
 import com.example.demo.repository.ArticleRepository;
 import com.example.demo.repository.AuthorityRepository;
 import com.example.demo.repository.MemberRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Builder
 @Service
@@ -16,6 +20,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final ArticleRepository articleRepository;
     private final AuthorityRepository authorityRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public MemberDTO findById(Long Id){
         return memberRepository.findById(Id)
@@ -29,6 +34,23 @@ public class MemberService {
                 .name(member.getName())
                 .email(member.getEmail())
                 .build();
+    }
+
+    public MemberDTO create(MemberForm memberForm){
+
+        Member member = Member.builder()
+                .name(memberForm.getName())
+                .password(passwordEncoder.encode (memberForm.getPassword()))
+                .email(memberForm.getEmail())
+                .build();
+
+        memberRepository.save(member);
+        return mapToMemberDTO(member);
+    }
+
+    public Optional<MemberDTO> findByEmail(String email){
+        return memberRepository.findByEmail(email)
+                .map(this::mapToMemberDTO);
     }
 
 }
