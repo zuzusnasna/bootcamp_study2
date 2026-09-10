b179c8bbbeee99adac6d8e7075457ef2a4cf59fe
jusanghyeok <jsmh991102@gmail.com>
2026-09-10 11:45:42 +0900

    게시판 재 작성중

--- bulletin_board/src/main/java/com/example/demo/repository/ArticleRepository.java
+++ bulletin_board/src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Article;
 import com.example.demo.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Member , Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
     @Transactional
     void deleteAllByMember(Member member);
 }
--- bulletin_board/src/main/java/com/example/demo/repository/AuthorityRepository.java
+++ bulletin_board/src/main/java/com/example/demo/repository/AuthorityRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Authority;
 import com.example.demo.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface AuthorityRepository extends JpaRepository<Member, Long> {
-    List<Member> findByMember(Member member);
+    List<Authority> findByMember(Member member);
 }
